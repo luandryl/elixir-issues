d6fd71dee7ffe1f0c59a5b1fbdc3d24456e63eb1
Luan Andyl <luan.andryl@gmail.com>
2020-11-27 03:07:59 -0300

    :feat: parse json from api call

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -16,8 +16,23 @@ defmodule Issues.CLI do
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+      |> decode_data
+      |> remove_useless_fields
   end
 
+  def remove_useless_fields(issues_list) do
+    # todo
+  end 
+
+  def decode_data({:ok, body}), do: body |> JSON.decode 
+
+  def decode_data({:error, error}) do
+    IO.puts """
+      Error to fetch data from github #{error['message']}
+    """
+    System.halt(2)
+  end
+  
   def parse_args(argv) do
     OptionParser.parse(argv, switches: [ help: :boolean], aliases: [ h: :help ])
       |> elem(1)
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,17 +1,23 @@
 defmodule CliTest do
   use ExUnit.Case
   doctest Issues
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [ parse_args: 1, sort_into_decending_order: 1 ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
     assert parse_args(["--help", "anything"]) == :help
   end
-    test "three values returned if three given" do
+
+  test "three values returned if three given" do
     assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
   end
-    test "count is defaulted if two values given" do
+
+  test "count is defaulted if two values given" do
     assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
+  test "sort descending orders " do 
+    assert ['c', 'b', 'a'] == sort_into_decending_order(['a', 'b', 'c'])
   end
 end
 
